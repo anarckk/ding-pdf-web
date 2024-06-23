f39ee09557382888c8c11d3b256a8fe128a22a7c
anarckk <anarckk@gmail.com>
2024-06-23 17:39:25 +0800

    写错字了

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ding-pef-web/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ding-pdf-web/' : '/',
   plugins: [
     vue(),
   ],
